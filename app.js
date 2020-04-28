527a8decdbfd70dae19672da469c7ac73519014a
tanvir <mdtanvirislams@gmail.com>
2020-04-28 17:12:15 +0600

    error handling , fetching single post

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -8,6 +10,7 @@ const app = express();
 
 //app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());//application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,6 +20,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message })
+})
 
 mongoose.connect('mongodb+srv://tanvir:00000000@cluster0-w7tp8.mongodb.net/messages?retryWrites=true&w=majority')
 .then(result => {
--- routes/feed.js
+++ routes/feed.js
@@ -10,8 +10,14 @@ router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
 router.post('/post', [
-    body('title').trim().isLength({min: 5}),
-    body('content').trim().isLength({min: 5})
-], feedController.createPost)
+    body('title')
+        .trim()
+        .isLength({min: 7}),
+    body('content')
+        .trim()
+        .isLength({min: 5})
+], feedController.createPost);
+
+router.get('/post/:postId', feedController.getPost);
 
 module.exports = router;

043f6cef9ae78e8c98b521901f210ed93544b0c8
tanvir <mdtanvirislams@gmail.com>
2020-04-29 15:58:52 +0600

    validating tokens

--- routes/feed.js
+++ routes/feed.js
@@ -2,14 +2,15 @@ const express = require('express');
 const { body } = require('express-validator')
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', [
+router.post('/post', isAuth, [
     body('title')
         .trim()
         .isLength({min: 5}),
@@ -18,9 +19,9 @@ router.post('/post', [
         .isLength({min: 5})
 ], feedController.createPost);
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
-router.put('/post/:postId', [
+router.put('/post/:postId', isAuth, [
     body('title')
         .trim()
         .isLength({min: 5}),
@@ -29,6 +30,6 @@ router.put('/post/:postId', [
         .isLength({min: 5})
 ], feedController.updatePost); 
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;

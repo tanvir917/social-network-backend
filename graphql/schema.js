6a99975f646a21277dd39b8c401611dfff3b7bf5
tanvir <mdtanvirislams@gmail.com>
2020-05-02 18:14:36 +0600

    fetching posts

--- graphql/schema.js
+++ graphql/schema.js
@@ -24,6 +24,11 @@ module.exports = buildSchema(`
         userId: String!
     }
 
+    type PostData {
+        posts: [Post!]!
+        totalPosts: Int!
+    }
+
     input UserInputData {
         email: String!
         name: String!
@@ -38,6 +43,7 @@ module.exports = buildSchema(`
 
     type RootQuery {
         login(email: String!, password: String!): AuthData!
+        posts: PostData!
     }
 
     type RootMutation {
